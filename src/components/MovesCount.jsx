4ca60d72c2e5ff8d921bd69c8bef007e843bef34
Victoria Rodríguez <100727319+victoriarodriguezz@users.noreply.github.com>
2024-02-22 20:04:56 -0300

    feat(MovesCount): create component

---
+++ src/components/MovesCount.jsx
@@ -0,0 +1,20 @@
+const MovesCount = ({ hits, fails }) => {
+    return (
+        <div className="mb-5 ring-1 ring-gray-900/5 bg-white/65 rounded-lg grid grid-cols-2 divide-x shadow-lg">
+            <div className="py-2 px-5">
+                <dt className="inline text-center">
+                    <p className="text-3xl font-bold text-green-500">{hits}</p>
+                    <p>{hits === 1 ? 'hit' : 'hits'}</p>
+                </dt>
+            </div>
+            <div className="py-2 px-5">
+                <dt className="inline text-center">
+                    <p className="text-3xl font-bold text-red-600">{fails}</p>
+                    <p>{fails === 1 ? 'fail' : 'fails'}</p>
+                </dt>
+            </div>
+        </div>
+    );
+};
+
+export default MovesCount;
