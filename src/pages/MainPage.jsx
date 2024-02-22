d70494197222f42eef3cd72f8346c161bc50825c
Victoria Rodríguez <100727319+victoriarodriguezz@users.noreply.github.com>
2024-02-22 20:07:25 -0300

    feat(MainPage): add user name logic and show components

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,8 +1,30 @@
+import { useEffect, useState } from 'react';
+
+import UserName from "../components/mainPage/UserName";
+import MemoryGame from "../components/mainPage/MemoryGame";
+import useCache from '../hooks/useCache';
+
 const MainPage = () => {
+  const { userName, updateUserName } = useCache();
+  const [askUserName, setAskUserName] = useState(true);
+
+  const handleGoToPlay = (name) => {
+    updateUserName(name);
+    setAskUserName(false);
+  };
+
+  useEffect(() => {
+    if (userName) {
+      setAskUserName(false);
+    }
+  }, [userName]);
+
   return (
-    <>Main page</>
-  )
-}
+    <>
+      {askUserName ? <UserName goToPlay={handleGoToPlay} askUserName={askUserName} /> : <MemoryGame />}
+    </>
+  );
+};
 
 export default MainPage;
 
